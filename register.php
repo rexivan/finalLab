f0a6f90897c11ca0d6a24ea56b0b343468af76ee
rexivan <ivan@larssonrivera.se>
2015-05-22 11:40:01 +0200

    la till kod i register.php

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/stylesheet.css">
+		<h1> Registrering </h1>
+	</head>
+	<body>
+		<form id="formen" name ="minForm" method="post" action="inlogg_register.php">
+			<label for="email"><h1>Min email-adress</h1></label>
+				<input type="text" id="emailaddress2"name="emailaddress2">
+			<label for="losenord"> <h1>Mitt losenord</h1></label>
+				<input type="password" id="losenord" name="losenord"><br>
+				<br>
+			<input type="submit" name="Registrera" id="knappen">
+		</form>
+
+		
+
+
+	</body>
+</html>
