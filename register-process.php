c13316b85cbceb1210309b0a55c7b1b2a7fe8d31
rexivan <ivan@larssonrivera.se>
2015-05-23 13:32:54 +0200

    bla. fixat lösenordsbekräftelse
    gå ej att registrera sig om inte lösen matchar lösen bekrf.

--- register-process.php
+++ register-process.php
@@ -9,6 +9,7 @@ $dbname ="db_finallab";
 $varUsr = $_POST['form_usr'];
 $varEmailaddress = $_POST['form_eadress'];
 $varLosenord = $_POST['form_pw'];
+$varLosenord2 = $_POST['form_pw2'];
 $md5_password = md5($varLosenord);
 $salt = uniqid(mt_rand(), true);
 $phpArray = array();
@@ -40,7 +41,7 @@ if(! $conn){
 
 
 
-
+if($varLosenord == $varLosenord2){
 if(0===count($phpArray)) {
 $sql = "INSERT INTO db_usrs (db_eadress, db_pw, db_slt, db_usr) VALUES ('$varEmailaddress', '$md5_password', '$salt', '$varUsr')" ;
 echo "Registration successfull!";
@@ -48,6 +49,10 @@ echo "Registration successfull!";
 if(! mysqli_query($conn, $sql)){
 	die("Could not connect123:"  . mysqli_error($conn));
 }
+}
+}
+else{
+	echo "Passwords dont match! Hit 'BACKSPACE' and try again!";
 }
 //else {
 	//header('Location: inlogg_grej.php');
@@ -57,7 +62,6 @@ if(! mysqli_query($conn, $sql)){
 ?>
 
 <form action="login.php">
-			<input type="submit" value="Now take me to the login page!"
-	</body>
-
+			<input type="submit" value="Go to the login page">
+<br>
 </html>
